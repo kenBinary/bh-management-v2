e5e447830fdcd62b8cdb3c30b33c73ea0e75aa85
thennek0 <kennethbecaro0@gmail.com>
2024-02-07 13:33:00 +0800

    refactored to be more concise

--- src/components/room-management/RoomCard.tsx
+++ src/components/room-management/RoomCard.tsx
@@ -2,38 +2,44 @@ import {
     Card, CardHeader, CardBody, CardFooter,
     VStack, Image, Text, Button
 } from '@chakra-ui/react';
+
+import { RoomSchema } from '../../services/room-management/RoomServices';
 interface CardProps {
-    roomNumber: number;
-    roomStatus: string;
-    roomType: string;
-    roomFee: number;
-    openModal: () => void;
+    room: RoomSchema;
+    openAssignModal: () => void;
+    openRemoveModal: () => void;
 }
-export default function RoomCard({ roomNumber, roomStatus, roomType, roomFee, openModal }: CardProps) {
+export default function RoomCard({ room, openAssignModal, openRemoveModal }: CardProps) {
     return (
         <Card flexShrink='0' direction="row" size="sm" width="full" overflow="hidden" padding="2" variant="outline">
             <Image flexShrink="0" boxSize="230px" overflow="hidden" src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
             <VStack width="full" align="start">
                 <CardHeader padding="2" fontSize="3xl" fontWeight="bold">
-                    {`${roomType}`}
+                    {`${room.room_type}`}
                 </CardHeader>
                 <CardBody display="grid" padding="2" gridTemplateColumns="1fr 1fr" gridTemplateRows="1fr 1fr" width="full">
                     <Text fontSize="xl">
-                        {`Room Number: ${roomNumber}`}
+                        {`Room Number: ${room.room_number}`}
                     </Text>
                     <Text fontSize="xl">
-                        {`Room Status: ${roomStatus}`}
+                        {`Room Status: ${room.room_status}`}
                     </Text>
                     <Text fontSize="xl">
-                        {`Room Type: ${roomType}`}
+                        {`Room Type: ${room.room_type}`}
                     </Text>
                     <Text fontSize="xl">
-                        {`Room Fee: ${roomFee} ₱`}
+                        {`Room Fee: ${room.room_fee} ₱`}
                     </Text>
                 </CardBody>
                 <CardFooter padding="2" gap="2" alignSelf="end">
-                    <Button onClick={openModal}>Asign Tenant</Button>
-                    <Button>Remove Tenant</Button>
+                    {
+                        (room.is_full)
+                            ?
+                            null
+                            :
+                            <Button onClick={openAssignModal}>Asign Tenant</Button>
+                    }
+                    <Button onClick={openRemoveModal}>Remove Tenant</Button>
                 </CardFooter>
             </VStack>
         </Card>
