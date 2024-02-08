23983aad1a2ddaf07a562967279f5ceab648a4b2
thennek0 <kennethbecaro0@gmail.com>
2024-02-08 21:14:59 +0800

    added site color pallete theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { HashRouter } from 'react-router-dom';
 
+const theme = extendTheme({
+  colors: {
+    brandPallete: {
+      background: "#393E46",
+      primary: "#219C90",
+      secondary: "#676B7C",
+      accent: "#00ADB5",
+      text: "#EEEEEE",
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
+
   <React.StrictMode>
     <HashRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </HashRouter>
