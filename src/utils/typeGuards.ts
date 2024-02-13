408576c739134222d14a8e4902a58314fae32e84
thennek0 <kennethbecaro0@gmail.com>
2024-02-13 15:30:07 +0800

    feat: added type guard for necessity bills

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,6 +1,13 @@
-import { RoomUtilityBill } from "../services/payment-management/paymentServices";
+import {
+    RoomUtilityBill, NecessityBill,
+} from "../services/payment-management/paymentServices";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isRoomUtilityBill(obj: any): obj is RoomUtilityBill {
     return 'room_number' in obj && 'total_bill' in obj && 'bill_due' in obj && 'date_paid' in obj && 'payment_status' in obj && 'room_utility_bill_id' in obj;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isNecessityBill(obj: any): obj is NecessityBill {
+    return 'total_bill' in obj && 'bill_due' in obj && 'date_paid' in obj && 'payment_status' in obj && 'necessity_bill_id' in obj;
 }
