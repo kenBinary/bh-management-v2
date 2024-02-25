f2751df13e293730dd6fe3c0ebfd9244bc237f75
thennek0 <kennethbecaro0@gmail.com>
2024-02-25 15:02:26 +0800

    feat: fetch room overview and yearly cash in fucntion

--- src/services/dashboardServices.ts
+++ src/services/dashboardServices.ts
@@ -17,3 +17,39 @@ export async function getPropertyMetrics(): Promise<CardMetrics | "fail"> {
         return "fail";
     }
 }
+
+export interface RoomOverview {
+    name: string;
+    value: number;
+}
+
+export async function getRoomOverview(): Promise<Array<RoomOverview> | "fail"> {
+    const url = `http://localhost:3000/analytics/room-overview`;
+    const response = await fetch(url, {
+        method: "GET"
+    });
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        return "fail";
+    }
+}
+
+export interface YearlyCashIn {
+    name: string;
+    value: number;
+}
+
+export async function getYearlyCashIn(): Promise<Array<YearlyCashIn> | "fail"> {
+    const url = `http://localhost:3000/analytics/yearly-cash-in`;
+    const response = await fetch(url, {
+        method: "GET"
+    });
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        return "fail";
+    }
+}
